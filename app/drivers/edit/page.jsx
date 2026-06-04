5891b1b597bf6331f085b193949914345e07aa61
Adeep Rana <adeeprana99999@gmail.com>
2026-06-04 16:56:52 -0400

    Fix reports and details pages

--- app/drivers/edit/page.jsx
+++ app/drivers/edit/page.jsx
@@ -2,6 +2,7 @@
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 export const revalidate = 0;
+
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { getData, putData } from "../../../lib/api";
