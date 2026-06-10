7ab8aaad187626eab95716344633e6107b1aba3a
adeeprana99999-byte <adeeprana99999@gmail.com>
2026-06-10 16:58:21 -0400

    Update page.jsx

--- app/drivers/page.jsx
+++ app/drivers/page.jsx
@@ -76,7 +76,7 @@ export default function DriversPage() {
   form.append("upload_preset", "fleetmind"); // create this in Cloudinary
 
   const res = await fetch(
-    "https://api.cloudinary.com/v1_1/dpekjdygn/upload",
+    "https://res.cloudinary.com/dpekjdygn/image/upload",
     { method: "POST", body: form }
   );
 
